3e8fb4dbc6f6dcdd32979bb1fbdade9d8641b911
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2023-09-04 15:11:52 +0300

    end

--- test-form-front/src/components/Form.tsx
+++ test-form-front/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 interface FormData {
     email: string
@@ -9,39 +9,50 @@ interface FormProps {
     searchData: (formData: FormData) => void
 }
 
-const Form: React.FC<FormProps> = ( {searchData} ) => {
-
-    const [formData, serFormData] = useState<FormData>( {email: '', number: ''} )
+const Form: React.FC<FormProps> = ({ searchData }) => {
+    const [formData, setFormData] = useState<FormData>({ email: '', number: '' })
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
         searchData(formData)
     }
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target
-        serFormData({...formData, [name]: value})
+        const { name, value } = e.target
+
+        if (name === 'number') {
+            const formattedValue = value.replace(/-/g, '')
+
+            const formattedNumber = formattedValue
+                .replace(/\D/g, '')
+                .replace(/(\d{2})(\d{2})(\d{2})/, '$1-$2-$3')
+
+            setFormData({ ...formData, [name]: formattedNumber })
+        } else {
+            setFormData({ ...formData, [name]: value })
+        }
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>Email: </label>
-            <input type='text'
-                   name='email'
-                   value={formData.email}
-                   onChange={handleChange}
-                   required
+            <input
+                type='text'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+                required
             />
             <label>Number: </label>
-            <input type='text'
-                   name='number'
-                   value={formData.number}
-                   onChange={handleChange}
+            <input
+                type='text'
+                name='number'
+                value={formData.number}
+                onChange={handleChange}
             />
-            <button type='submit'>
-                Search
-            </button>
+            <button type='submit'>Search</button>
         </form>
-    );
-};
+    )
+}
 
 export default Form;
